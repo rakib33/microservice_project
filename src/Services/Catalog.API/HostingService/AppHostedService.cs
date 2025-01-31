60f87ab1cc5d4f87a0df4093d34c41a434b26cac
rakibbjit <islam.rakibul@bjitgroup.com>
2025-01-31 11:58:37 +0600

    run code once to add seed data into database at project start.

---
+++ src/Services/Catalog.API/HostingService/AppHostedService.cs
@@ -0,0 +1,31 @@
+﻿
+using Catalog.API.Context;
+
+namespace Catalog.API.HostingService
+{
+    public class AppHostedService : IHostedService
+    {
+        /// <summary>
+        ///This method run once when application start and seed the database
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            CatalogDbContextSeed.Seed();
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
