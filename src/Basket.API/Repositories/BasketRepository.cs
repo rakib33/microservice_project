689be7e7d51504c2cb4347b2cce1e98faca94e83
rakibbjit <islam.rakibul@bjitgroup.com>
2025-02-02 15:15:58 +0600

    Implementing a repository pattern with redis Basket.API

---
+++ src/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Models;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+     private readonly IDistributedCache _radisCache;
+        public BasketRepository(IDistributedCache radisCache)
+        {
+            _radisCache = radisCache;
+        }
+
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _radisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(userName))
+            {
+                return  null;
+            }
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _radisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _radisCache.RemoveAsync(userName);
+        }
+    }
+}
