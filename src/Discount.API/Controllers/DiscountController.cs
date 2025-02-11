02ca65f8cb9a07af26f593c24fef5df1ef6484c5
rakibbjit <islam.rakibul@bjitgroup.com>
2025-02-11 15:27:12 +0600

    add discount controller

--- src/Discount.API/Controllers/DiscountController.cs
+++ src/Discount.API/Controllers/DiscountController.cs
@@ -1,6 +1,9 @@
 ﻿using CoreApiResponse;
+using Discount.API.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Discount.API.Models;
 
 namespace Discount.API.Controllers
 {
@@ -8,7 +11,85 @@ namespace Discount.API.Controllers
     [ApiController]
     public class DiscountController : BaseController
     {
+        ICouponRepository _couponRepository;
 
+        public DiscountController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetDiscount(string productId)
+        {
+            try
+            {
+                var coupon = await _couponRepository.GetDiscount(productId);
+                return CustomResult(coupon);
+            }
+            catch(Exception ex) {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CreateDiscount([FromBody] Coupon coupon)
+        {
+            try
+            {
+                var isSaved = await _couponRepository.CreateDiscount(coupon);
+                if(isSaved)
+                {
+                    return CustomResult(coupon);
+                }
+                return CustomResult("Coupon saved failed",coupon,HttpStatusCode.BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            try
+            {
+                var isSaved = await _couponRepository.UpdateDiscount(coupon);
+                if (isSaved)
+                {
+                    return CustomResult(coupon);
+                }
+                return CustomResult("Coupon update failed", coupon, HttpStatusCode.BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteDiscount([FromBody] Coupon coupon)
+        {
+            try
+            {
+                var isDeleted = await _couponRepository.UpdateDiscount(coupon);
+                if (isDeleted)
+                {
+                    return CustomResult("Coupon has been deleted.");
+                }
+                return CustomResult("Coupon deleted failed.", coupon, HttpStatusCode.BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
 
     }
 }
