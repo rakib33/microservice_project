2e2444b57bafd63243a7f6233aaf91d4a24fd0cf
rakibbjit <islam.rakibul@bjitgroup.com>
2025-02-11 12:09:32 +0600

    Add discount api model,repository and controller

---
+++ src/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,14 @@
+﻿using CoreApiResponse;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DiscountController : BaseController
+    {
+
+
+    }
+}
---
+++ src/Discount.API/Repository/ICouponRepository.cs
@@ -0,0 +1,14 @@
+﻿using Discount.API.Models;
+
+namespace Discount.API.Repository
+{
+    public interface ICouponRepository
+    {
+
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+
+    }
+}
