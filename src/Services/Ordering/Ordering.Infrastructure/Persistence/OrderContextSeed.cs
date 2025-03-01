144d3040de7bac5bfad27aadc0cda387dcad8f72
Rakibul Islam <rakib33mbstu@gmail.com>
2025-03-02 05:48:04 +0600

    add context and seed data

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().HasData(
+                new Order()
+                {
+                    Id = 1,
+                    UserName = "faisalcse3@gmail.com",
+                    FirstName = "Md",
+                    LastName = "Faisal",
+                    EmailAddress = "faisalcse3@gmail.com",
+                    Address = "Dhaka",
+                    TotalPrice = 100,
+                    City = "Dhaka",
+                    CVV = "Test",
+                    CardName = "Test",
+                    CardNumber = "Test",
+                    Expiraton = "Test",
+                    PaymentMethod = 1,
+                    CreatedBy = "Test",
+                    CreatedDate = DateTime.Now,
+                    PhoneNumber = "01700000000",
+                    State = "Test",
+                    ZipCode = "Test"
+                }
+                );
+
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+        }
+
+        protected override async void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            await OrderContextSeed.Seed(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
