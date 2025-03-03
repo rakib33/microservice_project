0487b02d83bdece6a9fba44af7ce430a97a3083d
Rakibul Islam <rakib33mbstu@gmail.com>
2025-03-03 10:59:21 +0600

    add migration remove dynamic seed warning

--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Ordering.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,18 @@ namespace Ordering.Infrastructure.Persistence
     {
         public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
         {
+        }
+
+        /// <summary>
+        /// HasData is used for seeding data, and EF Core is noticeing that the data is changing every time the application starts.
+        /// Avoid EF Core warning for dynamic seed data like this.new DateTime(), Guid.NewGuid()).'
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           // base.OnConfiguring(optionsBuilder);
+            optionsBuilder.ConfigureWarnings(warnings =>
+           warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
         }
 
         protected override async void OnModelCreating(ModelBuilder modelBuilder)
