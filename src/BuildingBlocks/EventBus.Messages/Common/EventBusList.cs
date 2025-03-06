7a15672a40d0406d5a36e3fe3d97470d1194602e
Rakibul Islam <rakib33mbstu@gmail.com>
2025-03-06 22:44:41 +0600

    Developing BuildingBlocks EventBus.Messages Class Library Project for RabbitMQ

---
+++ src/BuildingBlocks/EventBus.Messages/Common/EventBusList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Common
+{
+    /// <summary>
+    /// add queue as constant. Here only one queue is declare
+    /// </summary>
+    public static class EventBusList
+    {
+        public const string BasketChecoutQueue = "basketcheckout-queue";
+    }
+}
